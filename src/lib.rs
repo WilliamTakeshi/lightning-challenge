c0689659dd0d932b95a9591966c7281a9e64c546
William Takeshi <williamtpereira@gmail.com>
2024-09-28 14:16:55 -0300

    feat: add lib configuration

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use axum::{http::StatusCode, response::IntoResponse, routing::get, serve::Serve, Router};
+use tokio::net::TcpListener;
+
+pub fn run(listener: TcpListener) -> Serve<Router, Router> {
+    tracing_subscriber::fmt::init();
+    
+    let app = Router::new().route("/health_check", get(health_check));
+    axum::serve(listener, app)
+}
+
+async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-use axum::{
-    http::StatusCode, response::IntoResponse, routing::{get, post}, Json, Router
-};
-// use serde::{Deserialize, Serialize};
+use lightning_challenge::run;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
-
-    let app = Router::new()
-        .route("/health_check", get(health_check));
-
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    run(listener).await.unwrap();
 }
-
-async fn health_check() -> impl IntoResponse {
-    StatusCode::OK
-}
-
