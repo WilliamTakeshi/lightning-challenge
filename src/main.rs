351db069efe502f8a44366c72d4be14f5b51ad02
William Takeshi <williamtpereira@gmail.com>
2024-09-28 14:10:22 -0300

    feat: remove hello world, add healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    Json, Router,
+    http::StatusCode, response::IntoResponse, routing::{get, post}, Json, Router
 };
 // use serde::{Deserialize, Serialize};
 
@@ -10,13 +8,13 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
-        .route("/", get(root));
+        .route("/health_check", get(health_check));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
 }
 
